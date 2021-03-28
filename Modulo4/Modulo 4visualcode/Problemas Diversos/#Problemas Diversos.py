0e3ef5717e0c299c264f13a7851630c3de3caade
Jean A lexis <55679018+jeancitojdx@users.noreply.github.com>
2021-03-28 12:44:34 -0500

    Modulo4

---
+++ Modulo4/Modulo 4visualcode/Problemas Diversos/#Problemas Diversos.py
@@ -0,0 +1,33 @@
+#Problemas Diversos
+#1
+
+n = int(input('Introduce un número entero entre 1 y 10: '))
+file_name = 'tabla-' + str(n) + '.txt'
+f = open(file_name, 'w')
+for i in range(1, 11):
+    f.write(str(n) + ' x ' + str(i) + ' = ' + str(n * i) + '\n')
+f.close()
+
+#2 
+
+n = int(input('Introduce un número entero entre 1 y 10: '))
+file_name = 'tabla-' + str(n) + '.txt'
+try: 
+    f = open(file_name, 'r')
+except FileNotFoundError:
+    print('No existe el fichero con la tabla del', n)
+else:
+    print(f.read())
+
+#3
+
+n = int(input('Introduce un número entero entre 1 y 10: '))
+m = int(input('Introduce otro número entero entre 1 y 10: '))
+file_name = 'tabla-' + str(n) + '.txt'
+try: 
+    f = open(file_name, 'r')
+except FileNotFoundError:
+    print('No existe el fichero con la tabla del ', n)
+else:
+    lines = f.readlines()
+    print(lines[m - 1])
